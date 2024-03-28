93e8c3b16389c12a3bb9b036758202545e4124ab
Md Mahfoozur Rahman <tushersuvro@gmail.com>
2024-03-28 16:00:12 +0600

    issue form setup as well as saving and updating of issue form data with IssueForm livewire component

--- app/Livewire/Issues/IssueForm.php
+++ app/Livewire/Issues/IssueForm.php
@@ -2,12 +2,75 @@
 
 namespace App\Livewire\Issues;
 
+use App\Models\Client;
+use App\Models\Issue;
+use Illuminate\Http\RedirectResponse;
+use Livewire\Features\SupportRedirects\Redirector;
 use Livewire\Component;
+use Illuminate\Contracts\View\View;
 
 class IssueForm extends Component
 {
-    public function render()
+    public ?Issue $issue = null;
+
+    public string $title= '';
+    public string|null $description = '';
+    public int|null $client_id = null;
+
+    public bool $editing = false; // tracking form whether it is in create or edit mode
+
+    public $clients;
+
+    // using this method to load issue first if the form is in edit mode
+    public function mount(Issue $issue): void
+    {
+        $this->issue = $issue; //dd($this->issue);
+
+        if ($this->issue->exists) {
+            $this->editing = true;
+            $this->title = $issue->title;
+            $this->description = $issue->description;
+            $this->client_id = $issue->client_id;
+        }
+
+        $this->clients = Client::orderBy('name','asc')->get();
+    }
+
+    // this will be default load view method for this livewire component
+    public function render(): View
     {
         return view('livewire.issues.issue-form');
+    }
+
+    // this method will be used after form is submitted in livewire component
+    public function save(): Redirector|RedirectResponse
+    {
+        $this->validate(); //dd('inside save');
+
+        if ( !$this->editing ) {
+            $this->issue = Issue::create($this->only(['title', 'description', 'client_id']));
+        } else {
+            $this->issue->update($this->only(['title', 'description', 'client_id']));
+        }
+
+        return to_route('issues');
+    }
+
+    // rules that will be used in livewire component during form submission
+    protected function rules(): array
+    {
+        return [
+            'title' => [
+                'string',
+                'required',
+            ],
+            'description' => [
+                'string',
+                'required',
+            ],
+            'client_id' => [
+                'required',
+            ]
+        ];
     }
 }
--- resources/views/livewire/issues/issue-form.blade.php
+++ resources/views/livewire/issues/issue-form.blade.php
@@ -1,3 +1,38 @@
 <div>
-    {{-- Success is as dangerous as failure. --}}
+    <h1>{{ $editing ? 'Edit Client Issue ' : 'Create Client Issue' }}</h1>
+    <div class="card mt-3">
+        <div class="card-body">
+            <form wire:submit="save">
+                <div class="mb-3">
+                    <label for="title">Issue title</label>
+                    <input wire:model="title" id="title" class="form-control" type="text" name="title" required />
+                    @if ($errors->has('title'))
+                        <span class="text-danger">{{ $errors->first('title') }}</span>
+                    @endif
+                </div>
+                <div class="mb-3">
+                    <label for="email">Description:</label>
+                    <textarea  class="form-control" rows="5" id="description" name="description" wire:model="description"></textarea>
+                    @if ($errors->has('description'))
+                        <span class="text-danger">{{ $errors->first('description') }}</span>
+                    @endif
+                </div>
+                <div class="mb-3">
+                    <label for="pwd">Client:</label>
+                    <select id="client_id" class="form-control" wire:model="client_id">
+                        <option value="" selected>-- Choose client --</option>
+                        @foreach( $clients as $client )
+                            <option value="{{ $client->id }}" >{{ $client->name }}</option>
+                        @endforeach
+                    </select>
+                    @if ($errors->has('client_id'))
+                        <span class="text-danger">{{ $errors->first('client_id') }}</span>
+                    @endif
+                </div>
+                <button type="submit" class="btn btn-primary cursor-not-allowed">
+                    <span>Save</span>
+                </button>
+            </form>
+        </div>
+    </div>
 </div>
